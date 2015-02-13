629a629a6ac8d7859d2fe34c48e457353466d31f
Eric Mason <eric.mason@yupiq.com>
2015-02-13 11:48:56 -0800

    EM playing with mixed ember/play api routes

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,47 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-import views.html.*;
+import play.Routes;
+import play.libs.F.Function0;
+import play.libs.F.Promise;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
 
     public static Result index() {
         return ok(index.render());
     }
+    
+    public Promise<Result> getPhotos() {
+		return Promise.promise(new Function0<Result>(){
+			@Override
+			public Result apply() throws Throwable {
+				List<Photo> photos = new ArrayList<Photo>();
+				photos.add(new Photo("Tomster 7", "http://emberjs.com/images/about/ember-productivity-sm.png"));
+				photos.add(new Photo("Eiffel Tower 4", "http://emberjs.com/images/about/ember-structure-sm.png"));
+				photos.add(new Photo("Eiffel Tower 5", "http://emberjs.com/images/about/ember-structure-sm.png"));
+				return ok(Json.toJson(photos));
+			}
+		});
+    }
+    
+    private static class Photo implements Serializable {
+    	public String title;
+    	public String url;
+    	public Photo(String title, String url) {
+			this.title = title;
+			this.url = url;
+    	}
+    }
 
+	public Result javascriptRoutes() {
+		response().setContentType("text/javascript");
+		return ok(Routes.javascriptRouter("jsRoutes", controllers.routes.javascript.Application.getPhotos()));
+	}
 }
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,7 +4,31 @@ App.SubView = Ember.View.extend({
   templateName: "sub/sub"
 });
 
-App.Router = Ember.Router.extend({
+App.PhotosView = Ember.View.extend({
+	template: Ember.TEMPLATES.photos
+});
+
+App.Router.map(function() {
+	this.resource('photos', { path: '/photos' })
+});
+
+App.PhotosRoute = Ember.Route.extend({
+	model: function() {
+		console.log("Get data from route");
+		return jsRoutes.controllers.Application.getPhotos().ajax({ dataType : "json" });
+		/* console.log(jsRoutes.controllers.Application.getPhotos().getJSON())
+		console.log('Has routes: ' + jsRoutes.controllers.Application);
+		return [{
+			title: "Tomster",
+			url: "http://emberjs.com/images/about/ember-productivity-sm.png"
+		}, {
+			title: "Eiffel Tower",
+			url: "http://emberjs.com/images/about/ember-structure-sm.png"
+		}]; */
+	}
+});
+
+/* App.Router = Ember.Router.extend({
   root: Ember.Route.extend({
     index: Ember.Route.extend({
       route: '/',
@@ -13,4 +37,4 @@ App.Router = Ember.Router.extend({
       }
     })
   })
-});
+}); */
